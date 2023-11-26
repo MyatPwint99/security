4a39cb8b94ea24f6cd4b212312eff7e2c555b3f5
MyatPwint99 <myatpwintphyu99mpp@gmail.com>
2023-11-26 11:31:42 +0630

    security test1

---
+++ src/main/java/com/ojtbatch10/security/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.ojtbatch10.security.config;
+
+import com.ojtbatch10.security.bl.service.login.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    LoginService loginService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers( "/register", "/logout").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form ->
+                                form.loginPage("/login")
+//                                        .usernameParameter("email")
+//                                        .passwordParameter("password")
+//                                .successForwardUrl("redirect:/home")
+                                        .defaultSuccessUrl("/home", true)
+                                        .failureForwardUrl("/login?error=true")
+                                        .permitAll()
+                ).logout(form -> form.permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
